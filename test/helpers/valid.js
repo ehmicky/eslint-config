d98cd8c355cea361545533ec00d879e473957158
ehmicky <ehmicky@gmail.com>
2019-02-17 10:00:00 +0100

    Add smoke test

---
+++ test/helpers/valid.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const test = true
+
+module.exports = {
+  test,
+}
--- test/main.js
+++ test/main.js
@@ -1,7 +1,14 @@
 'use strict'
 
 const test = require('ava')
+const execa = require('execa')
 
-test('Dummy test', t => {
-  t.pass()
+const ESLINT_CONFIG = `${__dirname}/../dist/.eslintrc.json`
+const TEST_FILE = `${__dirname}/helpers/valid.js`
+
+test('Smoke test', async t => {
+  const { code, stdout, stderr } = await execa.shell(
+    `eslint ${TEST_FILE} --config ${ESLINT_CONFIG}`,
+  )
+  t.snapshot({ code, stdout, stderr })
 })
