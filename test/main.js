4152f3aee97b70e095ec4091becbc89830b97847
ehmicky <ehmicky@gmail.com>
2019-03-02 10:00:00 +0100

    Update testing

--- test/main.js
+++ test/main.js
@@ -3,7 +3,7 @@
 const test = require('ava')
 const execa = require('execa')
 
-const ESLINT_CONFIG = `${__dirname}/../build/src/.eslintrc.json`
+const ESLINT_CONFIG = `${__dirname}/../src/.eslintrc.json`
 const TEST_FILE = `${__dirname}/helpers/valid.js`
 
 test('Smoke test', async t => {
