961a2bbed3fa11bca1f5c786e93e47ff87603e56
ehmicky <ehmicky@gmail.com>
2019-03-26 10:00:00 +0100

    Improve test

--- test/main.js
+++ test/main.js
@@ -7,8 +7,10 @@ const ESLINT_CONFIG = `${__dirname}/../src/.eslintrc.json`
 const TEST_FILE = `${__dirname}/../../test/helpers/valid.js`
 
 test('Smoke test', async t => {
-  const { code, stdout, stderr } = await execa.shell(
-    `eslint ${TEST_FILE} --config ${ESLINT_CONFIG}`,
-  )
+  const { code, stdout, stderr } = await execa('eslint', [
+    TEST_FILE,
+    '--config',
+    ESLINT_CONFIG,
+  ])
   t.snapshot({ code, stdout, stderr })
 })
