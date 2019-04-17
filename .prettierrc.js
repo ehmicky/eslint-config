1a9bfe629e4411496a414ef3c57e8374bd6ee219
ehmicky <ehmicky@gmail.com>
2019-04-17 10:00:00 +0200

    Try fixing Windows tests

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,23 +5,8 @@
 // as the package's main file. We need to do that so it can be a shareable
 // configuration.
 module.exports = {
-  // Same as .editorconfig (and also default values)
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-
-  // Same as default values
-  quoteProps: 'as-needed',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: 'avoid',
-  htmlWhitespaceSensitivity: 'css',
-
-  // Different from default values
   semi: false,
   singleQuote: true,
-  jsxSingleQuote: true,
   trailingComma: 'all',
   loglevel: 'warn',
-  proseWrap: 'always',
 }
