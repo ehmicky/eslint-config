523e8bac27b9969d027773c0c583f26e723f84e6
ehmicky <ehmicky@gmail.com>
2020-03-22 19:13:38 +0100

    Update to latest Prettier

--- test/main.js
+++ test/main.js
@@ -4,7 +4,7 @@ import test from 'ava'
 const ESLINT_CONFIG = `${__dirname}/../../.eslintrc.yml`
 const TEST_FILE = `${__dirname}/../../test/helpers/valid.js`
 
-test('Smoke test', async t => {
+test('Smoke test', async (t) => {
   const { exitCode, stdout, stderr } = await execa(
     'eslint',
     [TEST_FILE, '--config', ESLINT_CONFIG],
