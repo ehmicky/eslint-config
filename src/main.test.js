ff4a6dedf6aa1d386bb31ef0b95d286eedafd632
ehmicky <ehmicky@gmail.com>
2022-12-04 23:24:08 +0100

    Refactoring

--- src/main.test.js
+++ src/main.test.js
@@ -6,15 +6,17 @@ import { execa } from 'execa'
 const ESLINT_CONFIG = fileURLToPath(
   new URL('../../.eslintrc.yml', import.meta.url),
 )
-const TEST_FILE = fileURLToPath(
-  new URL('../../src/helpers/valid.test.js', import.meta.url),
+const FIXTURE_FILE = fileURLToPath(
+  new URL('../../src/fixtures/valid.test.js', import.meta.url),
 )
 
 test('Smoke test', async (t) => {
   const { exitCode, stdout, stderr } = await execa(
     'eslint',
-    [TEST_FILE, '--config', ESLINT_CONFIG],
+    [FIXTURE_FILE, '--config', ESLINT_CONFIG],
     { preferLocal: true },
   )
-  t.snapshot({ exitCode, stdout, stderr })
+  t.is(exitCode, 0)
+  t.is(stdout, '')
+  t.is(stderr, '')
 })
