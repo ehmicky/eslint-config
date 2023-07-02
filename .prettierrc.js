b1df6563717aa9a12e6d27732990802bc6e7e151
ehmicky <ehmicky@gmail.com>
2023-07-02 15:23:42 +0100

    Use ES modules with Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,8 +2,7 @@
 // We use JavaScript instead of JSON or YAML so that this file can be exported
 // as the package's main file. We need to do that so it can be a shareable
 // configuration.
-// Prettier config file does not support ES modules yet.
-module.exports = {
+export default {
   // Same as .editorconfig (and also default values)
   printWidth: 80,
   tabWidth: 2,
